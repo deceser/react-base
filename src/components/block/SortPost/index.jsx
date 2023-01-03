e6ef3ffa2e0e5606012f4d4899d452c091c73706
deceser <aufhaummtobsc@gmail.com>
2023-01-03 10:26:10 +0200

    sort select post

---
+++ src/components/block/SortPost/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import SelectUi from "../../UI/SelectUi";
+
+const SortPost = ({ filter, setFilter }) => {
+  return (
+    <div>
+      <SelectUi
+        value={filter.sort}
+        onChange={(sortSelect) => setFilter({ ...filter, sort: sortSelect })}
+        defaultvalue="Sort"
+        options={[
+          { value: "title", name: "Sort title" },
+          { value: "content", name: "Sort description" },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default SortPost;
